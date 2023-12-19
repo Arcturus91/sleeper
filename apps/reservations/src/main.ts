046b36caf4f72164f3694600d2a5b4ca4e836fed
arturo-zarzilla <arturo-zarzilla@io2mail.com>
2023-12-19 13:23:10 -0500

    implement logger and class validator

--- apps/reservations/src/main.ts
+++ apps/reservations/src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { ReservationsModule } from './reservations.module';
+import { ValidationPipe } from '@nestjs/common';
+import { Logger } from 'nestjs-pino';
 
 async function bootstrap() {
   const app = await NestFactory.create(ReservationsModule);
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true })); //this is for not allowing other properties than the ones that are defined in the DTO
+  app.useLogger(app.get(Logger));
   await app.listen(3000);
 }
 bootstrap();
