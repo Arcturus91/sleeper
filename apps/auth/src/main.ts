608cef40097e9810c8ceb0dd475303903b495761
arturo-zarzilla <arturo-zarzilla@io2mail.com>
2024-01-10 19:22:20 -0500

    implement auth microserv and user module

--- apps/auth/src/main.ts
+++ apps/auth/src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AuthModule } from './auth.module';
+import { ValidationPipe } from '@nestjs/common';
+import { Logger } from 'nestjs-pino';
 
 async function bootstrap() {
   const app = await NestFactory.create(AuthModule);
-  await app.listen(3000);
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true })); //this is for not allowing other properties than the ones that are defined in the DTO
+  app.useLogger(app.get(Logger));
+  await app.listen(3001);
 }
 bootstrap();
--- apps/auth/src/users/users.service.ts
+++ apps/auth/src/users/users.service.ts
@@ -1,10 +1,13 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
+import { UsersRepository } from './users.repository';
 
 @Injectable()
 export class UsersService {
+  constructor(private readonly usersRepository: UsersRepository) {}
+
   async create(createUser: CreateUserDto) {
-    return 'hey, created';
+    return this.usersRepository.create(createUser);
   }
 }
 
