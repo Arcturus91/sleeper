10f24f80c804924b2b15a69b4a914634c8be6358
arturo-zarzilla <arturo-zarzilla@io2mail.com>
2024-01-12 19:50:45 -0500

    create local strategy method

--- apps/auth/src/users/users.service.ts
+++ apps/auth/src/users/users.service.ts
@@ -1,13 +1,27 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UsersRepository } from './users.repository';
+import * as bcrypt from 'bcryptjs';
 
 @Injectable()
 export class UsersService {
   constructor(private readonly usersRepository: UsersRepository) {}
 
+  async verifyUser(email: string, password: string) {
+    const user = await this.usersRepository.findOne({ email });
+    const passwordIsValid = await bcrypt.compare(password, user.password);
+
+    if (!passwordIsValid) {
+      throw new UnauthorizedException('Credentials are not valid');
+    }
+    return user;
+  }
+
   async create(createUser: CreateUserDto) {
-    return this.usersRepository.create(createUser);
+    return this.usersRepository.create({
+      ...createUser,
+      password: await bcrypt.hash(createUser.password, 10),
+    });
   }
 }
 
