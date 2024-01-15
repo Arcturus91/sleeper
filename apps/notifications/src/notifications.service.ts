ccadf9e6054ed64f9b0f0767446f77710c70d4f2
Arturo Barrantes Vasquez <arturo.barrantesv@pucp.pe>
2024-01-15 08:54:36 -0500

    implemente nodemailer transporter

--- apps/notifications/src/notifications.service.ts
+++ apps/notifications/src/notifications.service.ts
@@ -1,8 +1,10 @@
 import { Injectable } from '@nestjs/common';
 import { NotifyEmailDto } from './dto/notify-email.dto';
+import nodemailer from 'nodemailer';
 
 @Injectable()
 export class NotificationsService {
+  private readonly transporter = nodemailer.createTransport;
   async notifyEmail(data: NotifyEmailDto) {
     console.log('email', data);
   }
