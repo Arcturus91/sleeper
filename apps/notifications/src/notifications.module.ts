7b81cab102efeb1899fc11adfec4a4c3de0e2282
Arturo Barrantes Vasquez <arturo.barrantesv@pucp.pe>
2024-02-10 15:16:49 -0500

    implement auth nodeport

--- apps/notifications/src/notifications.module.ts
+++ apps/notifications/src/notifications.module.ts
@@ -11,7 +11,7 @@ import { LoggerModule } from '@app/common';
     ConfigModule.forRoot({
       isGlobal: true,
       validationSchema: Joi.object({
-        /*         PORT: Joi.number().required(), */
+        PORT: Joi.number().required(),
         SMTP_USER: Joi.string().required(),
         GOOGLE_OAUTH_CLIENT_ID: Joi.string().required(),
         GOOGLE_OAUTH_CLIENT_SECRET: Joi.string().required(),
