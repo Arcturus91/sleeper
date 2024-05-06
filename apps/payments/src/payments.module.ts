8430eaf263eb84561414ea236fa8ff9e277115de
Arturo Barrantes Vasquez <arturo.barrantesv@pucp.pe>
2024-05-06 16:44:49 -0500

    fix payments PORT_TCP ref. 100% operational

--- apps/payments/src/payments.module.ts
+++ apps/payments/src/payments.module.ts
@@ -12,7 +12,7 @@ import { ClientsModule, Transport } from '@nestjs/microservices';
     ConfigModule.forRoot({
       isGlobal: true,
       validationSchema: Joi.object({
-        PORT: Joi.number().required(),
+        PORT_TCP: Joi.number().required(),
         STRIPE_SECRET_KEY: Joi.string().required(),
         NOTIFICATIONS_HOST: Joi.string().required(),
         NOTIFICATIONS_PORT: Joi.number().required(),
